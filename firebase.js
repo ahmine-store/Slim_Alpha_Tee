78e5ec6b14179df208cc1882b393353fdda7c70e
ahmine-store <servicesahmine@gmail.com>
2026-01-21 10:26:16 -0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,19 @@
+// firebase.js (FOR HTML + JS WEBSITE)
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyCZX2N-hoZNL8V-xnzBuSOBmqYKITGwmhg",
+  authDomain: "slim-alpha-tee.firebaseapp.com",
+  projectId: "slim-alpha-tee",
+  storageBucket: "slim-alpha-tee.firebasestorage.app",
+  messagingSenderId: "368415138960",
+  appId: "1:368415138960:web:70490781c2f3e9fd556415"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore Database
 export const db = getFirestore(app);
